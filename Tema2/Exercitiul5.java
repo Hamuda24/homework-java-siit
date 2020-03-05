8c5372d0e2ba0c06e85350b4448f15485fab8158
Hamuda24 <61165328+Hamuda24@users.noreply.github.com>
2020-03-05 14:29:07 +0200

    Add files via upload

---
+++ Tema2/Exercitiul5.java
@@ -0,0 +1,28 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Exercitiul5 {
+    public static void main(String args[])
+    {
+
+        int[] arr = { 1, 5, 7, 3, 5, 3, -1 };
+        int sum = 6;
+        getPairsCount(arr, sum);
+    }
+
+
+    public static void getPairsCount(int[] arr, int sum)
+    {
+
+        int count = 0;//
+
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i + 1; j < arr.length; j++)
+                if ((arr[i] + arr[j]) == sum)
+                    count++;
+
+        System.out.printf("Count of pairs is %d",count);
+    }
+}
+
